8d265c6d01da786034c5055fa498968235336699
nithin-gith <nithin.siriguppa@gmail.com>
2022-08-18 17:26:32 +0530

    basic search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import style from './styles/app.module.css'
 import {courses_data} from './courses_data_for_react.js'
-import React, {useState} from 'react';
+import React, {useState,useRef} from 'react';
+import ListItem from './components/list_item';
+import Fuse from 'fuse.js'
 
 function App() {
+  
+  const inputBox= useRef(null)
+  const resultbox = useRef(null)
+  const options= {
+    shouldSort: true,
+    keys:['name','course_code'],
+    useExtendedSearch: true,
+    includeScore: true,
+  }
+  const fuse= new Fuse(courses_data,options)
+  const [result,setresult]=useState(fuse.search([]))
+  function handlekeyup(){
+    const results=fuse.search(inputBox.current.value)
+    setresult(results)
+  }
 
+  // console.log(resultbox.current.value)
   return (
     <div className="App">
       <div >Grade Estimator</div>
       <div>ENTER COURSE</div>
       <div className={style.inputbox} >
-          <input className={style.input}  />
+          <input placeholder="Search Course Here" onKeyUp={handlekeyup}className={style.input} ref={inputBox}  />
           <div >
-              <ul>
-              {courses_data.map((course)=>{
-                  return (<li key ={course.id}>{course.name} - {course.credits}</li>)
-              })}
+              <ul ref ={resultbox} className={style.result}>
+                  {result.length===0?(<ListItem name = "No Results Found!"/>):result.map((course) => {
+                      return <ListItem there = '1' key = {course.item.id} code ={course.item.course_code} name={course.item.name} credits={course.item.credits} />
+                  })}
               </ul>
           </div>
       </div>
---
+++ src/components/list_item.js
@@ -0,0 +1,20 @@
+import style from '../styles/list_item.module.css'
+
+export default function ListItem(props){
+    if(props.there ==='1'){
+        return(
+            <div className={style.list_item}>
+            <a href='#'  className={style.list_link}>
+                <div onClick={props.functi} className={style.link_div}>{props.code} - {props.name} ({props.credits})</div>
+            </a>
+            </div>
+        )
+    }else{
+        return(
+            <a href='#'  className={style.list_link}>
+                <div  className={style.link_div}> {props.name} </div>
+            </a>
+        )
+    }
+    
+}
